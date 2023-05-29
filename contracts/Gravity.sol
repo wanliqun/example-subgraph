805c3d3edca9f8b9f16953cb0e518b5c74d32945
Conflux <conflux@ConfluxdeMacBook-Pro-3.local>
2023-05-29 16:13:03 +0800

    add conflux eSpace example subgraph

--- contracts/Gravity.sol
+++ contracts/Gravity.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.8.9;
 
 contract GravatarRegistry {
   event NewGravatar(uint id, address owner, string displayName, string imageUrl);
@@ -15,22 +15,24 @@ contract GravatarRegistry {
   mapping (uint => address) public gravatarToOwner;
   mapping (address => uint) public ownerToGravatar;
 
-  function createGravatar(string _displayName, string _imageUrl) public {
+  function createGravatar(string memory _displayName, string memory _imageUrl) public {
     require(ownerToGravatar[msg.sender] == 0);
-    uint id = gravatars.push(Gravatar(msg.sender, _displayName, _imageUrl)) - 1;
 
+    uint id = gravatars.length;
+    gravatars.push(Gravatar(msg.sender, _displayName, _imageUrl));
+    
     gravatarToOwner[id] = msg.sender;
     ownerToGravatar[msg.sender] = id;
 
     emit NewGravatar(id, msg.sender, _displayName, _imageUrl);
   }
 
-  function getGravatar(address owner) public view returns (string, string) {
+  function getGravatar(address owner) public view returns (string memory, string memory) {
     uint id = ownerToGravatar[owner];
     return (gravatars[id].displayName, gravatars[id].imageUrl);
   }
 
-  function updateGravatarName(string _displayName) public {
+  function updateGravatarName(string memory _displayName) public {
     require(ownerToGravatar[msg.sender] != 0);
     require(msg.sender == gravatars[ownerToGravatar[msg.sender]].owner);
 
@@ -40,7 +42,7 @@ contract GravatarRegistry {
     emit UpdatedGravatar(id, msg.sender, _displayName, gravatars[id].imageUrl);
   }
 
-  function updateGravatarImage(string _imageUrl) public {
+  function updateGravatarImage(string memory _imageUrl) public {
     require(ownerToGravatar[msg.sender] != 0);
     require(msg.sender == gravatars[ownerToGravatar[msg.sender]].owner);
 
@@ -48,16 +50,5 @@ contract GravatarRegistry {
 
     gravatars[id].imageUrl =  _imageUrl;
     emit UpdatedGravatar(id, msg.sender, gravatars[id].displayName, _imageUrl);
-  }
-
-  // the gravatar at position 0 of gravatars[]
-  // is fake
-  // it's a mythical gravatar
-  // that doesn't really exist
-  // dani will invoke this function once when this contract is deployed
-  // but then no more
-  function setMythicalGravatar() public {
-    require(msg.sender == 0x8d3e809Fbd258083a5Ba004a527159Da535c8abA);
-    gravatars.push(Gravatar(0x0, " ", " "));
   }
 }
---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    evmtestnet: {
+      url: "https://evmtestnet.confluxrpc.com/09K6dkMRr9xyz6suyNgqTR",
+      accounts: ["6d61120eace48811c3f64d0a75599b43f4e61207d4c7802e7b2dacbb486d4fda"],
+    }
+  },
+};
