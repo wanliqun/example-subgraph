384b871ae39d58391739410ddc0e465b559ba7d2
wanliqun <wanliqunn@163.com>
2023-06-01 14:52:53 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: "0.8.18",
   networks: {
     evmtestnet: {
-      url: "https://evmtestnet.confluxrpc.com/09K6dkMRr9xyz6suyNgqTR",
+      url: "https://evmtestnet.confluxrpc.com",
       accounts: ["6d61120eace48811c3f64d0a75599b43f4e61207d4c7802e7b2dacbb486d4fda"],
     }
   },
